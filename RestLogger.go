ba9b5fd18052082d50c1b8099674b3628f18ca8e
Jojii <jojii@jojii.de>
2019-11-14 20:05:10 +0100

    log time only if neccessary

--- RestLogger.go
+++ RestLogger.go
@@ -11,6 +11,13 @@ func Logger(inner http.Handler, name string) http.Handler {
 		LogInfo(r.Method + " " + r.RequestURI + " " + name)
 		start := time.Now()
 		inner.ServeHTTP(w, r)
-		LogInfo("Duration: " + time.Since(start).String())
+		dur := time.Since(start)
+		if dur > 500*time.Millisecond && dur < 1500*time.Millisecond {
+			LogInfo("Duration: " + dur.String())
+		} else if dur > 1500*time.Millisecond && dur < 2500*time.Millisecond {
+			LogError("Duration: " + dur.String())
+		} else if dur > 2500*time.Millisecond {
+			LogCritical("Duration: " + dur.String())
+		}
 	})
 }
