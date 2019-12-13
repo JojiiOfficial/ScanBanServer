ee994889ae15ddf3cacb08aaac9e8a4587ea902e
jojiiofficial <jojii@jojii.de>
2019-12-13 17:17:55 +0100

    updated time to unix for blockedIPs

--- Queries.go
+++ Queries.go
@@ -206,7 +206,7 @@ func insertIP(ipdata IPData, uid uint) (IPid uint, reportID uint, err error) {
 	if err != nil {
 		return
 	}
-	err = execDB("INSERT INTO BlockedIP (ip, validated) VALUES (?,0) ON DUPLICATE KEY UPDATE reportCount=reportCount+1, deleted=0", ipdata.IP)
+	err = execDB("INSERT INTO BlockedIP (ip, validated,firstReport, lastReport) VALUES (?,0,(SELECT UNIX_TIMESTAMP(),(SELECT UNIX_TIMESTAMP())) ON DUPLICATE KEY UPDATE reportCount=reportCount+1, deleted=0", ipdata.IP)
 	if err != nil {
 		return
 	}
